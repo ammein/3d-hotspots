de1d96645ba4997422e2e794cd00e986a53a2843
Amin Shazrin <aminshazrin@yahoo.com>
2025-08-29 19:10:29 +0800

    Done

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,5 @@
-import { withThemeByClassName, withThemeByDataAttribute } from '@storybook/addon-themes';
-// import 'tailwindcss/index.css';
 import '@/theme.css'
 
-export const decorators = [
-  withThemeByClassName({
-    themes: {
-      light: 'light',
-      dark: 'dark',
-    },
-    defaultTheme: 'light',
-  }),
-  withThemeByDataAttribute({
-    themes: {
-      light: 'light',
-      dark: 'dark',
-    },
-    defaultTheme: 'light',
-    attributeName: 'data-mode',
-  })
-];
 
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
